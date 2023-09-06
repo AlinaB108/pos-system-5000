390bc05c3b8ce60a71823731696db5da29252f7c
Christopher Banta <chris.banta@rocketmail.com>
2023-09-06 17:09:27 -0500

    Made Category model

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const Menu = require('./Menu');
 const Roles = require('./Roles');
 const Shift = require('./Shift');
 const Table = require('./Table');
+const Category = require('./Category');
 
-module.exports = { Employee, Shift, Menu, Roles, Table };
+module.exports = { Employee, Shift, Menu, Roles, Table, Category};
