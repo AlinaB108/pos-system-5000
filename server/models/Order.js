464e53ae015a793b3ee011d0f15191e646150791
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-09-06 14:22:03 -0500

    added order model

---
+++ server/models/Order.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    method: {
+        type: String,
+        required: true
+    },
+    items: [Menu.schema],
+    time: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order; 
