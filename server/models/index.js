4a7cf98a4bd4ef2df467ca5d81cbcd9e69d78045
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-09-06 14:43:44 -0500

    added inventory model

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Employee = require('./Employee');
+const Inventory = require('./Inventory');
+const Menu = require('./Menu');
+const Order = require('./Order');
+const Table = require('./Table');
+
+module.exports = { Employee, Inventory, Menu, Order, Table };
