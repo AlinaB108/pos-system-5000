80e4565a2102fa89db3391f71edea4850715e7f4
Alina Badillo <alinachristabel108@gmail.com>
2023-09-07 16:47:04 -0500

    Numpad is working and responsive

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import PosLoginKeyPad from '../../components/PosLoginKeyPad/PosLoginKeyPad';
+import { AppBar, Toolbar, Typography, Button, Hidden } from '@mui/material';
+
 
 const Login = () => {
   return (
     <div className='container'>
+      <NavLogin/>
       <PosLoginKeyPad />
     </div>
+  );
+}
+
+function NavLogin() {
+  return (
+    <AppBar position="static" style={{ backgroundColor: '#d4e1f1' }}>
+      <Toolbar sx={{ py: 2 }}>
+        <Typography variant="h5" style={{ textDecoration: 'none', color: '#23296c' }}>
+          Logo
+        </Typography>
+        <Hidden smDown>
+          <div style={{ marginLeft: 'auto' }}>
+            <Typography variant="h3" style={{ textDecoration: 'none', color: '#23296c' }}>
+              SIDEKICK
+            </Typography>
+          </div>
+        </Hidden>
+        <div style={{ marginLeft: 'auto' }}>
+          <Button variant="contained" href="/login" color="error" size="large">
+            EXIT
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
