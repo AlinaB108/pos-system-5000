5effe52df25e4a4bd380c7ec00fa3d19fd59fd32
Alina Badillo <alinachristabel108@gmail.com>
2023-09-08 17:49:53 -0500

    Added tables, coworkers and sections; need to fix mobile screen size

--- client/src/components/PosServerProfile/PosServerProfile.js
+++ client/src/components/PosServerProfile/PosServerProfile.js
@@ -1,14 +1,102 @@
 import React from 'react';
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-
+import { Box, Grid, Paper, Typography } from '@mui/material';
+// import Button from '@mui/material/Button';
 
 function PosServerProfile() {
   return (
-    <Grid container direction="row" alignItems="center" justifyContent="center" height="50vh" display="flex"> 
-    something
+    <Grid container justifyContent="center" alignItems="center" sx={{ mt: 3 }}>
+      {/* First Container */}
+      <Grid item xs={12} sm={6}>
+        <Grid container justifyContent="center" alignItems="stretch" height='70vh'>
+          <Grid item xs={11}>
+            <Typography variant="h5" textAlign='center' sx={{ p: 2, backgroundColor: "#d4e1f1" }}>
+              OPEN MENUS
+            </Typography>
+            <Grid container justifyContent="center" spacing={3}>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 1
+                  </Typography>
+                  <Typography sx={{ ml: 2 }}>
+                    <p>aaaaaaaaa</p>
+                    <p>aaaaaaaaa</p>
+                    <p>aaaaaaaaa</p>
+                    <p>aaaaaaaaa</p>
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 2
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 3
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 4
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 5
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} md={4}>
+                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    Table 6
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* Second Container */}
+      <Grid item xs={12} sm={6}>
+        <Grid container justifyContent="center" alignItems="stretch" height='70vh'>
+          <Grid item xs={6} sm={6}>
+            <Paper sx={{ marginRight: '20px' }}>
+              <Typography variant="h5" textAlign='center' sx={{ p: 2, backgroundColor: "#d4e1f1" }}>
+                Coworkers
+              </Typography>
+              <Grid sx={{ p: 2 }} style={{ overflowY: 'auto' }}>
+                <p>You</p>
+                <p>Not You</p>
+                <p>Not You x2</p>
+              </Grid>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6} sm={6}>
+            <Paper sx={{ marginRight: '20px' }}>
+              <Typography variant="h5" textAlign='center' sx={{ p: 2, backgroundColor: "#d4e1f1" }}>
+                Sections
+              </Typography>
+              <Grid sx={{ p: 2 }} style={{ overflowY: 'auto' }}>
+                <p>20,21,22</p>
+                <p>30,31,32</p>
+                <p>40,41,42</p>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './Profile.css';
 import PosNav from '../../components/PosNav/PosNav';
-import PosManagerProfile from '../../components/PosManagerProfile/PosManagerProfile';
+// import PosManagerProfile from '../../components/PosManagerProfile/PosManagerProfile';
+import PosServerProfile from '../../components/PosServerProfile/PosServerProfile';
 
 const Profile = () => {
   return (
     <div className="container">
       <PosNav />
-      <PosManagerProfile />
+      {/* <PosManagerProfile /> */}
+      <PosServerProfile />
     </div>
   );
 };
