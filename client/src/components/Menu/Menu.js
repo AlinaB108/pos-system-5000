108f8f171a09b2e370aacde84a3a21adb157f9a3
Alina Badillo <alinachristabel108@gmail.com>
2023-09-11 17:23:46 -0500

    Main landing page structure is done; working on details

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,15 +1,25 @@
 import React from "react";
-// import { useQuery } from '@apollo/client';
-// import { useStoreContext } from '../../utils/GlobalState';
-// import { QUERY_CATEGORIES } from '../../utils/queries';
-// import { idbPromise } from '../../utils/helpers';
-import './Menu.css'
+import { Typography, Grid } from '@mui/material';
+import rest2 from "../../assets/rest2.jpg";
 
 function Menu() {
   return (
-    <div>
-      <h2>Menu section</h2>
-    </div>
+    <Grid container direction="row" style={{ marginTop: "50px" }} id="Menu">
+      <Grid container item xs={12} md={6} alignItems="center" justifyContent="center" sx={{ px: 2 }}>
+        <Grid sx={{ pt: 2 }} textAlign='right'>
+          <Typography variant="h3" sx={{ my:1 }}>
+            Menu
+          </Typography>
+          <Typography variant="h6">
+            Our menu is a delightful journey through Italy's diverse regions, featuring time-honored recipes passed down through generations. 
+            From the savory pasta dishes of Tuscany to the wood-fired pizzas of Naples, every bite is a taste of Italy's culinary heritage.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container item xs={12} md={6} alignItems="center" justifyContent="center" sx={{ px: 2 }}>
+        <img src={rest2} width='100%' height='auto' alt="A white plate with fish on a table" />
+      </Grid>
+    </Grid>
   );
 }
 
