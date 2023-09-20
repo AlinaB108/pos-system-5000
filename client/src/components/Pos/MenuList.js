a8e33178e021da2eaadb6d2da4c949eb2a91d980
Alina Badillo <alinachristabel108@gmail.com>
2023-09-20 18:45:59 -0500

    First container has a scrollbar now

--- client/src/components/Pos/MenuList.js
+++ client/src/components/Pos/MenuList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, Button, Typography } from '@mui/material';
+import { Box, Container, Grid, Button, Typography, ScopedCssBaseline } from '@mui/material';
 
 
 const MenuList = ({ menuItems }) => {
@@ -9,28 +9,40 @@ const MenuList = ({ menuItems }) => {
   }
 
   return (
-    <Grid container justifyContent="center" md={6}>
-      {menuItems.map(item => {
-        return <Grid container justifyContent="center" sx={{ mt: 2 }} item md={4} sm={9}>
-          <Grid>
-            <Button>
-                <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden', width: '300px' }} height='25vh' style={{ backgroundColor: "#fff" }}>
-                  <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
-                    {item.item}
-                  </Typography>
-                  <Typography color="#000" sx={{ pl: 1, pr: 1 }} height="fit-content">
-                    ${item.price}
-                    {item.ingredients.map(item => {
-                      return <div key={item._id}>
-                        <li>{item}</li>
-                      </div>
-                    })}
-                  </Typography>
-                </Box>
-              </Button>
+    <Grid container justifyContent="center" spacing={4}>
+      {/* First container */}
+      <Grid item xs={6}>
+        
+        <Grid style={{ maxHeight: '65vh', overflowY: 'auto', width: '100%' }}>
+          <Grid container justifyContent="center">
+            {menuItems.map(item => (
+              <Grid container justifyContent="center" sx={{ mt: 2 }} item md={4} sm={9} key={item.id}>
+                <Grid>
+                  <Button>
+                    <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden', width: '260px' }} height='25vh' style={{ backgroundColor: "#fff" }} >
+                      <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                        {item.item}
+                      </Typography>
+                      <Typography color="#000" sx={{ pl: 1, pr: 1 }} height="fit-content">
+                        ${item.price}
+                        {item.ingredients.map(ingredient => (
+                          <Grid key={ingredient._id}>
+                            <li>{ingredient}</li>
+                          </Grid>
+                        ))}
+                      </Typography>
+                    </Box>
+                  </Button>
+                </Grid>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
-      })}
+      </Grid>
+
+      {/* Second container */}
+      <Grid item xs={6}>
+      </Grid>
     </Grid>
   );
 };
