8e0d81f5aff16b5125075210328eff48f46332b5
Alina Badillo <alinachristabel108@gmail.com>
2023-09-20 23:43:17 -0500

    Centered text for tables and buttons

--- client/src/components/Pos/MenuList.js
+++ client/src/components/Pos/MenuList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Grid, Button, Typography, Paper } from '@mui/material';
+import { Box, Checkbox, Grid, Button, Typography, Paper } from '@mui/material';
 
 
 const MenuList = ({ menuItems }) => {
@@ -18,18 +18,20 @@ const MenuList = ({ menuItems }) => {
               <Grid container justifyContent="center" sx={{ mt: 2 }} item md={4} sm={9} key={item.id}>
                 <Grid>
                   <Button>
-                    <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden', width: '260px' }} height='25vh' style={{ backgroundColor: "#fff" }} >
-                      <Typography variant="h6" textAlign='center' sx={{ pt: 2, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
+                    <Box sx={{ m: 2, borderRadius: '25px', overflow: 'hidden', width: '260px' }} height='25vh' style={{ backgroundColor: "#fff" }}>
+                      <Typography variant="h6" textAlign='center' sx={{ p: 1, backgroundColor: "#fce698", borderRadius: '25px 25px 0 0' }}>
                         {item.item}
                       </Typography>
-                      <Typography color="#000" sx={{ pl: 1, pr: 1 }} height="fit-content">
-                        ${item.price}
-                        {item.ingredients.map(ingredient => (
-                          <Grid key={ingredient._id}>
-                            <li>{ingredient}</li>
-                          </Grid>
-                        ))}
-                      </Typography>
+                      <Box sx={{ pt:1 }}>
+                        <Typography color="#000" height="fit-content" lineHeight="3">
+                          ${item.price}
+                          {item.ingredients.map(ingredient => (
+                            <Grid key={ingredient._id}>
+                              <Typography sx={{listStyle: 'none'}}>{ingredient}</Typography>
+                            </Grid>
+                          ))}
+                        </Typography>
+                      </Box>
                     </Box>
                   </Button>
                 </Grid>
@@ -48,35 +50,47 @@ const MenuList = ({ menuItems }) => {
         </Grid> */}
           
         {/* 4 containers - actions */}
-        <Grid container item xs={6} spacing={3}>
+        <Grid container item xs={12} md={12} lg={6} spacing={3}>
           <Grid item xs={6}>
+            {/* Add this to Papers when make it a button onClick={() => handleButtonClick(1)} */}
             <Paper style={{ height: '100%' }}>
-              <Typography>Container 1</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <Typography>ADD Ingredient</Typography>
+              </Box>
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper style={{ height: '100%' }}>
-              <Typography>Container 2</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <Typography>Remove Ingredient</Typography>
+              </Box>
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper style={{ height: '100%' }}>
-              <Typography>Container 3</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <Typography>ITEM</Typography>
+              </Box>
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper style={{ height: '100%' }}>
-              <Typography>Container 4</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                <Typography>EDIT Price</Typography>
+              </Box>
             </Paper>
           </Grid>
         </Grid>
         {/* Ingredients container */}
-        <Grid item xs={6} sx={{ maxHeight: '65vh', px:3 }}>
+        <Grid item xs={12} md={12} lg={6} sx={{ maxHeight: '65vh', px:3 }}>
           <Paper style={{ height: '100%' }}>
-            <Typography>
+            <Typography textAlign="center" sx={{ p:2 }}>
               List of Selectable Ingredients:
             </Typography>
-            <li>ingredient</li>
+            {/* Might try checkbox? If yes I will style it globally */}
+            <Typography><Checkbox sx={{ color: '#23296c'}}/>bacon</Typography>
+            <Typography><Checkbox sx={{ color: '#23296c'}}/>tears</Typography>
+            <Typography><Checkbox sx={{ color: '#23296c'}}/>grapes</Typography>
           </Paper>
         </Grid>
       </Grid>
